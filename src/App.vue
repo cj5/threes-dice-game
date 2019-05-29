63c0577866440141720d85fbe8c83836a6f8b98b
Chris <stac1221@gmail.com>
2019-05-28 21:21:09 -0500

    initial setup— heading, 5 styled die that display a random value

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="heading-wrapper">
+        <h1>Threes</h1>
+        <h2>Dice game</h2>
+      </div>
+      <div class="gameboard">
+        <die />
+        <die />
+        <die />
+        <die />
+        <die />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import die from './components/die'
+
+export default {
+  name: 'app',
+  components: {
+    die
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './styles/main.scss';
+</style>
---
+++ src/components/die.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="die" :class="randDieValue">
+      <!-- <span>{{ randDieValue }}</span> -->
+      <span class="spot spot-1"></span>
+      <span class="spot spot-2"></span>
+      <span class="spot spot-3"></span>
+      <span class="spot spot-4"></span>
+      <span class="spot spot-5"></span>
+      <span class="spot spot-6"></span>
+      <span class="spot spot-7"></span>
+      <span class="spot spot-8"></span>
+      <span class="spot spot-9"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'die',
+  data() {
+    return {
+      dieValue: ''
+    }
+  },
+  computed: {
+    randDieValue() {
+      const randValue = Math.floor(Math.random() * 6) + 1
+      if (randValue == 1) {
+        return this.dieValue = 'one'
+      } else if (randValue == 2) {
+        return this.dieValue = 'two'
+      } else if (randValue == 3) {
+        return this.dieValue = 'three'
+      } else if (randValue == 4) {
+        return this.dieValue = 'four'
+      } else if (randValue == 5) {
+        return this.dieValue = 'five'
+      } else if (randValue == 6) {
+        return this.dieValue = 'six'
+      }
+    }
+  }
+}
+</script>
